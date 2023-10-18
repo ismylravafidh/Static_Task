fa178b1d280c79d334a23151ab2093c104ae61ec
ismylravafidh <bd82v6x9y@code.edu.az>
2023-10-18 11:09:06 +0400

    Static Task

---
+++ task/Models/ExtensionMethods.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticTask.Models
+{
+    internal static class ExtensionMethods
+    {
+        public static bool CustomContains(this string stringToSearch, char containsChar)
+        {
+            for (int i = 0; i < stringToSearch.Length; i++)
+            {
+                if (stringToSearch[i] == containsChar)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool IsPrime(this int Prime)
+        {
+            if (Prime <= 1)
+            {
+                return false;
+            }
+            if (Prime == 2)
+            {
+                return true;
+            }
+            for (int i = 2; i * i <= Prime; i++)
+            {
+                if (Prime % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsPowOfTwo(this int Pow)
+        {
+            int Power = 0;
+            if (Pow <= 1)
+            {
+                return false;
+            }
+            while (Pow >= 2)
+            {
+                if (Pow % 16 == 0)
+                {
+                    Power += 4;
+                    Pow /= 16;
+                }
+                else if (Pow % 8 == 0)
+                {
+                    Power += 3;
+                    Pow /= 8;
+                }
+                else if (Pow % 4 == 0)
+                {
+                    Power += 2;
+                    Pow /= 4;
+                }
+                else if (Pow % 2 == 0)
+                {
+                    Power++;
+                    Pow /= 2;
+                }
+                else
+                {
+                    Power *= 0;
+                    break;
+                }
+            }
+            if (Power == 0)
+            {
+                return false;
+            }
+            Console.WriteLine($"2^{Power}");
+            return true;
+        }
+    }
+}
---
+++ task/Program.cs
@@ -0,0 +1,21 @@
+﻿using StaticTask.Models;
+namespace StaticTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sozu dxil edin : ");
+            string Word = Console.ReadLine();
+            Console.Write("Axtarilacaq herfi daxil edin ,  eger soz daxil etsez ilk herf qebul edilecek : ");
+            char ContainsChar = Console.ReadLine()[0];
+            Console.WriteLine(Word.CustomContains(ContainsChar));
+            Console.Write("Yoxlanilacaq tam ededi daxil et : ");
+            int Prime = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Prime.IsPrime());
+            Console.Write("Yoxlanilacaq tam ededi daxil et : ");
+            int Pow = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Pow.IsPowOfTwo());
+        }
+    }
+}
